2cb536b0086e7bbf001691c4e98698e6fec07b75
Narokan <79106222+Nar0kan@users.noreply.github.com>
2021-07-10 10:34:37 +0300

    Delete Split_List_Into_Two.py

--- Split_List_Into_Two.py
+++
@@ -1,7 +0,0 @@
-def split_list(items: list) -> list:
-    if len(items) % 2 == 0:
-        return [items[:int(len(items)/2)], items[int(len(items)/2):]]
-    else:
-        return [items[:int(len(items)/2)+1], items[1+int(len(items)/2):]]
-
-print(split_list([1, 2, 3, 4, 5, 6, 7]))
